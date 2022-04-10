263223dbd2b78fd5a9dcb1e80b35547b41440a83
roll20plusplus <roll20plusplus@gmail.com>
2022-04-10 16:57:32 -0700

    Add cognito user pool

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-1_gR6PkOivn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6l1sjvrhlllj7f34bms2kolp7i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
