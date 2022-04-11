2ac4655e7277fc28c3f2ac893a49a7714576ab55
roll20plusplus <roll20plusplus@gmail.com>
2022-04-10 21:07:35 -0700

    Add character sheet support

--- js/scripts.js
+++ js/scripts.js
@@ -124,7 +124,7 @@ function init() {
     updateModifications();
 }
 
-var charSheetEl = $('open-character-sheet'),
+var charSheetButtonEl = $('open-character-sheet'),
   drawingModeEl = $('drawing-mode'),
   drawingOptionsEl = $('drawing-mode-options'),
   drawingColorEl = $('drawing-color'),
@@ -134,8 +134,22 @@ var charSheetEl = $('open-character-sheet'),
   drawingShadowOffset = $('drawing-shadow-offset'),
   clearEl = $('clear-canvas');
 
-charSheetEl.onclick = function () {
-    window.open('https://main.d38el35uq1kkh7.amplifyapp.com/charactersheet/');
+function load_charsheet() {
+     $("charsheet").innerHTML='<object class="container d-flex flex-column align-content-end h-100 w-100" type="text/html" data="5ecs.html" ></object>';
+}
+
+load_charsheet();
+
+charSheetButtonEl.onclick = function () {
+    charSheetEl = document.getElementById('dragcharsheet');
+    if (charSheetEl.style.visibility == 'hidden') {
+        charSheetButtonEl.innerHTML = 'Hide Character Sheet';
+        charSheetEl.style.visibility = 'visible';
+    }
+    else {
+        charSheetButtonEl.innerHTML = 'Show Character Sheet';
+        charSheetEl.style.visibility = 'hidden';
+    }
 }
 
 clearEl.onclick = function() { clearcan()};
@@ -465,7 +479,7 @@ canvas.on('mouse:wheel', function(opt) {
 document.onkeyup = KeyPress;
 
 dragElement(document.getElementById("dragcharsheet"));
-jQuery("#dragcharsheetheader").load("https://main.d38el35uq1kkh7.amplifyapp.com/charactersheets/");
+//jQuery("#dragcharsheetheader").load("https://main.d38el35uq1kkh7.amplifyapp.com/charactersheets/");
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -506,6 +520,83 @@ function dragElement(elmnt) {
     document.onmouseup = null;
     document.onmousemove = null;
   }
-
-
 }
+
+function makeResizableDiv(div) {
+  const element = document.querySelector(div);
+  const resizers = document.querySelectorAll(div + ' .resizer')
+  const minimum_size = 20;
+  let original_width = 0;
+  let original_height = 0;
+  let original_x = 0;
+  let original_y = 0;
+  let original_mouse_x = 0;
+  let original_mouse_y = 0;
+  for (let i = 0;i < resizers.length; i++) {
+    const currentResizer = resizers[i];
+    currentResizer.addEventListener('mousedown', function(e) {
+      e.preventDefault()
+      original_width = parseFloat(getComputedStyle(element, null).getPropertyValue('width').replace('px', ''));
+      original_height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
+      original_x = element.getBoundingClientRect().left;
+      original_y = element.getBoundingClientRect().top;
+      original_mouse_x = e.pageX;
+      original_mouse_y = e.pageY;
+      window.addEventListener('mousemove', resize)
+      window.addEventListener('mouseup', stopResize)
+    })
+
+    function resize(e) {
+      if (currentResizer.classList.contains('bottom-right')) {
+        const width = original_width + (e.pageX - original_mouse_x);
+        const height = original_height + (e.pageY - original_mouse_y)
+        if (width > minimum_size) {
+          element.style.width = width + 'px'
+        }
+        if (height > minimum_size) {
+          element.style.height = height + 'px'
+        }
+      }
+      else if (currentResizer.classList.contains('bottom-left')) {
+        const height = original_height + (e.pageY - original_mouse_y)
+        const width = original_width - (e.pageX - original_mouse_x)
+        if (height > minimum_size) {
+          element.style.height = height + 'px'
+        }
+        if (width > minimum_size) {
+          element.style.width = width + 'px'
+          element.style.left = original_x + (e.pageX - original_mouse_x) + 'px'
+        }
+      }
+      else if (currentResizer.classList.contains('top-right')) {
+        const width = original_width + (e.pageX - original_mouse_x)
+        const height = original_height - (e.pageY - original_mouse_y)
+        if (width > minimum_size) {
+          element.style.width = width + 'px'
+        }
+        if (height > minimum_size) {
+          element.style.height = height + 'px'
+          element.style.top = original_y + (e.pageY - original_mouse_y) + 'px'
+        }
+      }
+      else {
+        const width = original_width - (e.pageX - original_mouse_x)
+        const height = original_height - (e.pageY - original_mouse_y)
+        if (width > minimum_size) {
+          element.style.width = width + 'px'
+          element.style.left = original_x + (e.pageX - original_mouse_x) + 'px'
+        }
+        if (height > minimum_size) {
+          element.style.height = height + 'px'
+          element.style.top = original_y + (e.pageY - original_mouse_y) + 'px'
+        }
+      }
+    }
+
+    function stopResize() {
+      window.removeEventListener('mousemove', resize)
+    }
+  }
+}
+
+makeResizableDiv('.resizable')
