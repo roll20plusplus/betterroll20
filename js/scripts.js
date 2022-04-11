c86d330723ec980a1c919f95910543080e3acf5b
roll20plusplus <roll20plusplus@gmail.com>
2022-04-10 17:42:10 -0700

    Added cognito/amplify javascript and page routing

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,43 @@
 // Scripts
 //
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { Home, MainApp, Profile } from './pages';
+import { SignIn, SignUp } from './auth';
+import 'normalize.css';
+
+const isAuthenticated = () => false;
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (
+    isAuthenticated() === true
+      ? <Component {...props} />
+      : <Redirect to='/signin' />
+  )} />
+)
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/register" component={SignUp} />
+	        <Route path="/signin" component={SignIn} />
+          <Route path="/profile" component={Profile} />
+          <PrivateRoute path="/app" component={MainApp} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 var socket;
 
 var current;
