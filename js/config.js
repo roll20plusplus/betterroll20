ba63e4c360898716942e56c6ef6181b92f3c48c2
Ryan Malley <roll20plusplus@gmail.com>
2022-04-12 14:25:41 -0700

    update identity pool id in config file

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-1_bJ5HhIOsZ', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '758q6inh4n4kqhuaknabndn5fb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        identityPoolId: '_config.cognito.region',
+        identityPoolId: 'us-west-1:601dc2e5-45e5-49ba-8e58-797f317a077b',
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
