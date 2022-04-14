50ae9f76dee29e636b21108d72598f1a7d1659bf
Ryan Malley <roll20plusplus@gmail.com>
2022-04-14 10:21:47 -0700

    Add all of the headers

--- js/scripts.js
+++ js/scripts.js
@@ -145,7 +145,7 @@ function init() {
         "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
-        "X-Requested-With": "*"
+        "X-Requested-With": "*",
         "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS 
       'Content-Type': 'application/json',
       'Authorization': sessionToken
