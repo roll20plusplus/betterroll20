0da2708319bd211f67b6a436f67becb8efd77f16
Ryan Malley <roll20plusplus@gmail.com>
2022-04-14 10:50:34 -0700

    Instantiate message

--- js/scripts.js
+++ js/scripts.js
@@ -601,7 +601,7 @@ function loadCanvasState() {
             TableName: 'Archive',
             Key:{'messageID': 'Canvas', 'date': 'current'}
           };
-        docClient.get(params, function(err, data) {
+        var msg = docClient.get(params, function(err, data) {
             if (err) {
                 console.log("Error", err);
             } else {
