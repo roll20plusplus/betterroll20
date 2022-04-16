aae994013e38c010f9d5476e073c87a75792ab2d
roll20plusplus <103390875+roll20plusplus@users.noreply.github.com>
2022-04-15 18:38:29 -0800

    Davids dumb zoom fix

--- js/scripts.js
+++ js/scripts.js
@@ -637,15 +637,16 @@ function KeyPress(e) {
 }
 
 canvas.on('mouse:wheel', function(opt) {
-    var delta = opt.e.deltaY;
-    var zoom = canvas.getZoom();
-    zoom *= 0.999 ** delta;
-    if (zoom > 20) zoom = 20;
-    if (zoom < 0.01) zoom = 0.01;
-    canvas.setZoom(zoom);
-    opt.e.preventDefault();
-    opt.e.stopPropagation();
-})
+  var delta = opt.e.deltaY;
+  var zoom = canvas.getZoom();
+  zoom *= 0.999 ** delta;
+  if (zoom > 20) zoom = 20;
+  if (zoom < 0.01) zoom = 0.01;
+  canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+  opt.e.preventDefault();
+  opt.e.stopPropagation();
+});
+
 
 function getUserProfile() {
 
