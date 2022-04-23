8a7814a1581393d206e53b1422f9b5fb3875137f
roll20plusplus <roll20plusplus@gmail.com>
2022-04-23 15:59:46 -0700

    Put my sent messages back in the console

--- js/socketScripts.js
+++ js/socketScripts.js
@@ -15,32 +15,22 @@ function sendSocketMessage(type, username, contents) {
     switch (type) {
         case MessageType.CanvasUpdate:
             console.log("Canvas update going out to socket");
-            var msg = {
-                action: type,
-                data : {'sender': username, 'contents': contents}
-            };
-            socket.send(JSON.stringify(msg));
-            break;
         case MessageType.ChatMessage:
             console.log("Sending a chat message to the socket");
-            var msg = {
-                action : type,
-                data : {'sender': username, 'contents': contents}
-            };
-            socket.send(JSON.stringify(msg));
             break;
         case MessageType.BroadcastAction:
             console.log("Broadcasting an action to other users");
-            var msg = {
-                action : type,
-                data : {'sender': username, 'contents': contents}
-            };
-            socket.send(JSON.stringify(msg));
             break;
-
         default:
             console.log("Tried to send a message that was neither a Canvas Update or a Chat Message: " + type);
+            break;
+        };
     }
+    var msg = {
+        action: type,
+        data : {'sender': username, 'contents': contents}
+    console.log(msg);
+    socket.send(JSON.stringify(msg));
 }
 
 
