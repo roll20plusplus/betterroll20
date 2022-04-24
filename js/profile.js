bb6f580b9dd6d4cb67b95a66e14af3f4fa8a6f10
roll20plusplus <roll20plusplus@gmail.com>
2022-04-23 18:23:10 -0700

    Try and get the profile page back up and running

--- js/profile.js
+++ js/profile.js
@@ -1,2 +1,40 @@
 var userAttributes = getUserProfile();
 console.log(userAttributes);
+
+const UserProfileAttributes = {
+    Gender: "gender",
+    UserName: "preferred_username",
+    Email: "email",
+    EmailVerified: "email_verified",
+    FullName: "name"
+}
+
+function initUserAttributes() {
+    console.log("Getting and assigning user attribute values")
+    getUserProfile(function(result) {
+        console.log(result);
+        if (result == null) {
+            console.log('Couldnt get user attributes');
+            return;
+        }
+        for (i = 0; i < result.length; i++) {
+            switch(result[i].getName()) {
+                case UserProfileAttributes.Email:
+                    document.getElementByID("email").value = result[i].getValue();
+                    break;
+                case UserProfileAttributes.FullName:
+                    document.getElementByID("name").value = result[i].getValue();
+                    break;
+                case UserProfileAttributes.UserName:
+                    document.getElementByID("username").value = result[i].getValue();
+                    break;
+                case UserProfileAttributes.Gender:
+                    document.getElementByID("gender").value = result[i].getValue();
+                    break;
+                case UserProfileAttributes.EmailVerified:
+//                    userEmailVerified = result[i].getValue();
+                    break;
+            }
+        }
+    });
+}
